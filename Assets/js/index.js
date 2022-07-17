6573a6a896f6d74f34b3cf8c1e68776606efc500
MercyChebet <chebetm96@gmail.com>
2022-07-17 14:21:29 +0300

    Adding arguments

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -9,5 +9,11 @@ let totalProduce = 0;
 
 var no = "Litres Per Shed : <br>"; 
 var items= document.getElementsByName('shed'); 
-function chebet() { 
+function chebet() {  for (var i = 0; i < items.length; i++) { 
+    var a = items[i]; 
+    no = no +  a.value + " " +"<br>"; 
+} 
+document.getElementById("res").innerHTML = no; 
+document.getElementById("milk").innerHTML = "Productions"; 
+} 
   
