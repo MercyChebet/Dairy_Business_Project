c2e6a0c95d8e6c466d880b1e051490762f609396
MercyChebet <chebetm96@gmail.com>
2022-07-17 14:20:27 +0300

    Naming variables

---
+++ Assets/js/index.js
@@ -0,0 +1,13 @@
+const shedA = 510;
+const shedB = 308;
+const shedC = 486;
+const shedD = 572;
+
+let totalProduce = 0;
+
+
+
+var no = "Litres Per Shed : <br>"; 
+var items= document.getElementsByName('shed'); 
+function chebet() { 
+  
