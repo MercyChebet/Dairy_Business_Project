9f87a1ed5603b8509f4d9f5112498a6ed4ba7145
MercyChebet <chebetm96@gmail.com>
2022-07-17 21:37:45 +0300

    FInal js changes

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -4,10 +4,10 @@ function reportSummary(){
 
     let data = [];
 
-     data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
-     data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
-     data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
-     data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
+    data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+    data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+    data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+    data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
     let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 
